2bea12ac1f4c6ce5b40e0b5f6d2019b9b7e5f32e
Ilya <ilya.shuvatov1@gmail.com>
2022-11-08 08:15:55 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,14 @@
+const carName = 'BMW';
+let maxCarSpeed = 250;
+let carOwner = 'Ilya Shuvatov'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = 300;
+carOwner = 'Неизвестный тип'
+console.log('Чипанул любимую, теперь разгоняется до', maxCarSpeed)
+console.log('Продал малышку, теперь её владелец какой-то', carOwner)
+maxCarSpeed = 350
+console.log(carOwner + ' чипанул её еще разок, теперь она летит', maxCarSpeed)
